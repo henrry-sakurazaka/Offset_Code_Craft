478164c2327e6daee5aa4efdf8f5c3b31c407b2e
henrry-sakurazaka <bright.lights.21@me.com>
2025-08-03 13:59:46 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,45 +1,14 @@
-# Rails.application.routes.draw do
-#   root "pages#home", as: :home
+ Rails.application.routes.draw do
+    root "pages#home" # → root_path
 
-#   get "home", to: "pages#home", as: :home
-#   get "about", to: "pages#about", as: :about
-#   get "contact", to: "pages#contact", as: :contact
+    get "about", to: "pages#about", as: :about      # → about_path
+    get "contact", to: "pages#contact", as: :contact  # → contact_path
 
-#   # 送信完了画面
-#   get "contacts/complete", to: "contacts#complete_contact", as: :complete_contact
+    get "contacts/complete", to: "contacts#complete_contact", as: :complete_contact # → complete_contact_path
 
-#   # フォーム送信
-#   post "submit_contact", to: "contacts#create", as: :submit_contact
-#   post "contacts/create", to: "contacts#create"
-#   get "architex/home", to: "architex#home", as: :architex_home
+    post "contacts", to: "contacts#create", as: :submit_contact  # → submit_contact_path
 
-#   get "up", to: "rails/health#show", as: :rails_health_check
-# end
+    get "architex/home", to: "architex#home", as: :architex_home  # → architex_home_path
 
-# Rails.application.routes.draw do
-#   root "pages#home"
-
-#   get "about", to: "pages#about", as: :about
-#   get "contact", to: "pages#contact", as: :contact
-
-#   get "contacts/complete", to: "contacts#complete_contact", as: :complete_contact
-
-#   post "contacts", to: "contacts#create", as: :submit_contact
-
-#   get "architex/home", to: "architex#home", as: :architex_home
-
-#   get "up", to: "rails/health#show", as: :rails_health_check
-# end
-
-root "pages#home" # → root_path
-
-get "about", to: "pages#about", as: :about      # → about_path
-get "contact", to: "pages#contact", as: :contact  # → contact_path
-
-get "contacts/complete", to: "contacts#complete_contact", as: :complete_contact # → complete_contact_path
-
-post "contacts", to: "contacts#create", as: :submit_contact  # → submit_contact_path
-
-get "architex/home", to: "architex#home", as: :architex_home  # → architex_home_path
-
-get "up", to: "rails/health#show", as: :rails_health_check    # → rails_health_check_path
+    get "up", to: "rails/health#show", as: :rails_health_check    # → rails_health_check_path
+ end
