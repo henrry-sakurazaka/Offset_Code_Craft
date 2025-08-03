3976a48676a687be56a7f1992a8e508ebcaee2d2
henrry-sakurazaka <bright.lights.21@me.com>
2025-08-03 13:44:28 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,17 @@
 Rails.application.routes.draw do
-  # メインページ
   root "pages#home"
-  get "home", to: "pages#home"
-  get "about", to: "pages#about"
-  get "contact", to: "pages#contact"
-  get "pages/complete", to: "pages#complete"
 
-  # アーキテクチャ紹介
-  get "architex/home"
+  get 'pages/home', to: 'pages#home', as: :pages_home
+  get 'pages/about', to: 'pages#about', as: :pages_about
+  get 'pages/contact', to: 'pages#contact', as: :pages_contact
+  get 'pages/complete', to: 'pages#complete', as: :pages_complete
 
-  # Contacts 関連（new, create, complete）
-  get "contacts/new", to: "contacts#new"
+  get 'contacts/new', to: 'contacts#new', as: :contacts_new
+  post 'submit_contact', to: 'contacts#create', as: :submit_contact
   post "contacts/create", to: "contacts#create"
-  get "contacts/complete", to: "contacts#complete_contact", as: :complete_contact
-  # config/routes.rb
-  post "submit_contact", to: "contacts#create", as: :submit_contact
+  get 'contacts/complete', to: 'contacts#complete', as: :contacts_complete
 
-  # Health Check
+  get 'architex/home', to: 'architex#home', as: :architex_home
+
   get "up" => "rails/health#show", as: :rails_health_check
 end
