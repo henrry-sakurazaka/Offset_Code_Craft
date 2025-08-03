c19c0b4ed87c2399d1cc680353c09c60c481cd9b
henrry-sakurazaka <bright.lights.21@me.com>
2025-08-03 14:20:14 +0900

    latest version

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,14 @@
  Rails.application.routes.draw do
-    root "pages#home" # → root_path
+      root "pages#home"
 
-    get "about", to: "pages#about", as: :about      # → about_path
-    get "contact", to: "pages#contact", as: :contact  # → contact_path
+      get "home", to: "pages#home", as: :home
+      get "about", to: "pages#about", as: :about
+      get "contact", to: "pages#contact", as: :contact
 
-    get "contacts/complete", to: "contacts#complete_contact", as: :complete_contact # → complete_contact_path
+      get "contacts/complete", to: "contacts#complete_contact", as: :complete_contact
+      post "submit_contact", to: "contacts#create", as: :submit_contact
 
-    post "contacts", to: "contacts#create", as: :submit_contact  # → submit_contact_path
+      get "architex/home", to: "architex#home", as: :architex_home
 
-    get "architex/home", to: "architex#home", as: :architex_home  # → architex_home_path
-
-    get "up", to: "rails/health#show", as: :rails_health_check    # → rails_health_check_path
- end
+      get "up", to: "rails/health#show", as: :rails_health_check
+end
