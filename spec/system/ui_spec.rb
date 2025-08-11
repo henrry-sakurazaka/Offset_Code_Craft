ce9d7c5df82d0dda7de447519d4b3fded6bf349a
henrry-sakurazaka <bright.lights.21@me.com>
2025-08-12 00:28:12 +0900

    Update ui_spec.rb

--- spec/system/ui_spec.rb
+++ spec/system/ui_spec.rb
@@ -1,7 +1,7 @@
 RSpec.describe 'topページ', type: :system do
   it 'タイトルが表示される' do
     visit '/'
-    expect(page).to have_css('.catch', text: /Where Imagination Meets Possibility Unleashing the Power of Innovation/i)
+    expect(page).to have_css('.catch', text: /Where Imagination\s*Meets Possibility\s*Unleashing the Power of Innovation/i)
     expect(page).to have_css(".move-text",text: /portfolio 2023-2024/i, visible: :all)
   end
 
@@ -10,7 +10,7 @@ RSpec.describe 'topページ', type: :system do
     expect(page).to have_css("img[src*='listen']")
     expect(page).to have_css("img[src*='mute-svgrepo-com']")
     expect(page).to have_css("img[src*='pexels-laker-6157040']")
-    expect(page).to have_css("[style*='background-image'][style*='images_mountain/2.jpg']")
+    expect(page).to have_css("[style]", text: /background-image:\s*url\(.*images_mountain\/2\.jpg.*\)/)
     expect(page).to have_css("img[src*='works1.png']")
     expect(page).to have_css("img[src*='works3.png']")
     expect(page).to have_css("img[src*='works5.png']")
@@ -22,7 +22,7 @@ end
 RSpec.describe 'aboutページ', type: :system do
   it 'タイトルが表示される' do
     visit about_path
-    expect(page).to have_css(".catch", text: /Creating Boundless Wonders Shaping the Future AKIRA SAKAMOTO/i)  
+    expect(page).to have_css(".catch", text: /Creating Boundless\s*Wonders Shaping\s*the Future\s*AKIRA SAKAMOTO/i)  
   end
 
   it 'オーディオ装置のアイコン、画像が表示されている' do
@@ -30,7 +30,7 @@ RSpec.describe 'aboutページ', type: :system do
     expect(page).to have_css("img[src*='listen']")
     expect(page).to have_css("img[src*='mute-svgrepo-com']")
     expect(page).to have_css("img[src*='pexels-tara-winstead-6489631']")
-    expect(page).to have_css("[style*='background-image'][style*='images_mountain/2.jpg']")
+    expect(page).to have_css("[style]", text: /background-image:\s*url\(.*images_mountain\/2\.jpg.*\)/)
     expect(page).to have_css("img[src*='selfy.png']")
     expect(page).to have_css(".self-img-container", style: /background-image:.*brick-wall-room-architecture-indoor-concept\.jpg/)
   end
@@ -39,7 +39,7 @@ end
 RSpec.describe 'contactページ', type: :system do
   it 'タイトルが表示される' do
     visit contact_path
-    expect(page).to have_css(".text", text: /Feel free to drop me a message/i)
+    expect(page).to have_css(".text", text: /Feel free\s*to drop\s*me a\s*message/i)
   end
 
   it 'オーディオ装置のアイコン、画像が表示されている' do
