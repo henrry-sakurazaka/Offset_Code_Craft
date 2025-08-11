e51faf89d3d7ec459c7f93bdc1fb9b17de9bfe50
henrry-sakurazaka <bright.lights.21@me.com>
2025-08-11 21:07:07 +0900

    Update ci.yml rails_helper.rb

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -66,6 +66,7 @@ RSpec.configure do |config|
 
 Capybara.register_driver :chrome do |app|
   options = Selenium::WebDriver::Chrome::Options.new
+  options.add_argument('--headless')
   options.add_argument('--disable-gpu')
   options.add_argument('--no-sandbox')
   options.add_argument('--disable-dev-shm-usage')
