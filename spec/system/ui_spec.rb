f21715adce87b2384f9b551b776a367432404895
henrry-sakurazaka <bright.lights.21@me.com>
2025-08-12 01:17:02 +0900

    Update ui_spec.rb

--- spec/system/ui_spec.rb
+++ spec/system/ui_spec.rb
@@ -10,7 +10,7 @@ RSpec.describe 'topページ', type: :system do
     expect(page).to have_css("img[src*='listen']")
     expect(page).to have_css("img[src*='mute-svgrepo-com']")
     expect(page).to have_css("img[src*='pexels-laker-6157040']")
-    expect(page).to have_css(".catch[style]", text: /background-image:\s*url\(.*images_mountain\/2\.jpg.*\)/)
+    expect(page).to have_css(".catch[style*='images_mountain/2.jpg']")
     expect(page).to have_css("img[src*='works1.png']")
     expect(page).to have_css("img[src*='works3.png']")
     expect(page).to have_css("img[src*='works5.png']")
@@ -30,7 +30,7 @@ RSpec.describe 'aboutページ', type: :system do
     expect(page).to have_css("img[src*='listen']")
     expect(page).to have_css("img[src*='mute-svgrepo-com']")
     expect(page).to have_css("img[src*='pexels-tara-winstead-6489631']")
-    expect(page).to have_css(".catch[style]", text: /background-image:\s*url\(.*images_mountain\/2\.jpg.*\)/)
+    expect(page).to have_css(".catch[style*='images_mountain/2.jpg']")
     expect(page).to have_css("img[src*='selfy.png']")
     expect(page).to have_css(".self-img-container", style: /background-image:.*brick-wall-room-architecture-indoor-concept\.jpg/)
   end 
