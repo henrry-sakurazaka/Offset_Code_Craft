bd0be4595db5142101b49d2a45f0d0c23895ad10
henrry-sakurazaka <bright.lights.21@me.com>
2025-08-12 01:11:27 +0900

    Update ui_spec.rb

--- spec/system/ui_spec.rb
+++ spec/system/ui_spec.rb
@@ -1,7 +1,7 @@
 RSpec.describe 'topページ', type: :system do
   it 'タイトルが表示される' do
     visit '/'
-    expect(page).to have_css('.catch', text: /Where Imagination\s*Meets Possibility\s*Unleashing the Power of Innovation/i)
+    expect(page).to have_css('.catch', text: /Where Imagination\s*Meets Possibility\s*Unleashing the Power\s*of Innovation/i)
     expect(page).to have_css(".move-text",text: /portfolio 2023-2024/i, visible: :all)
   end
 
@@ -10,7 +10,7 @@ RSpec.describe 'topページ', type: :system do
     expect(page).to have_css("img[src*='listen']")
     expect(page).to have_css("img[src*='mute-svgrepo-com']")
     expect(page).to have_css("img[src*='pexels-laker-6157040']")
-    expect(page).to have_css("[style]", text: /background-image:\s*url\(.*images_mountain\/2\.jpg.*\)/)
+    expect(page).to have_css(".catch[style]", text: /background-image:\s*url\(.*images_mountain\/2\.jpg.*\)/)
     expect(page).to have_css("img[src*='works1.png']")
     expect(page).to have_css("img[src*='works3.png']")
     expect(page).to have_css("img[src*='works5.png']")
@@ -30,10 +30,10 @@ RSpec.describe 'aboutページ', type: :system do
     expect(page).to have_css("img[src*='listen']")
     expect(page).to have_css("img[src*='mute-svgrepo-com']")
     expect(page).to have_css("img[src*='pexels-tara-winstead-6489631']")
-    expect(page).to have_css("[style]", text: /background-image:\s*url\(.*images_mountain\/2\.jpg.*\)/)
+    expect(page).to have_css(".catch[style]", text: /background-image:\s*url\(.*images_mountain\/2\.jpg.*\)/)
     expect(page).to have_css("img[src*='selfy.png']")
     expect(page).to have_css(".self-img-container", style: /background-image:.*brick-wall-room-architecture-indoor-concept\.jpg/)
-  end
+  end 
 end
 
 RSpec.describe 'contactページ', type: :system do
