f96cce1af4dd4a8a08e9c406fd94860bc36fd283
henrry-sakurazaka <bright.lights.21@me.com>
2025-08-13 22:35:33 +0900

    Fix problem tests code

--- spec/rubocop_spec.rb
+++ spec/rubocop_spec.rb
@@ -11,7 +11,6 @@ RSpec.describe 'Code Quality and Navigation System Tests' do
       puts stderr unless stderr.empty?
       expect(status.success?).to be true
     end
-  end
 
   describe 'Navigation', type: :system do
     before { driven_by(:selenium_chrome) }
