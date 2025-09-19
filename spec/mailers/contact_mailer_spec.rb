356f9f5392e3b6af4ce19e6df8463d3c6bf754d5
henrry-sakurazaka <bright.lights.21@me.com>
2025-09-19 15:49:42 +0900

    Update contact_mailer_spec.rb

--- spec/mailers/contact_mailer_spec.rb
+++ spec/mailers/contact_mailer_spec.rb
@@ -6,57 +6,42 @@ RSpec.describe ContactMailer, type: :mailer do
   let(:name) { '山田太郎' }
   let(:email) { 'taro@example.com' }
   let(:message) { 'こんにちは' }
-  let(:mailjet_spy) { instance_spy(Mailjet::Send) }
+
+  let(:mailjet_spy) { class_double(Mailjet::Send).as_stubbed_const }
 
   before do
-    # 環境変数を設定
-    ENV['FROM_MAIL_ADDRESS'] = 'noreply@offsetcodecraft.site'
-    ENV['TO_MAIL_ADDRESS'] = 'admin@example.com'
-
-    # Mailjet::Send を spy にする
-    stub_const('Mailjet::Send', mailjet_spy)
+    allow(mailjet_spy).to receive(:create)
+    described_class.contact_email(name, email, message)
   end
 
   it '正しい宛先に送信されること' do
-    described_class.contact_email(name, email, message)
-    expect(Mailjet::Send).to have_received(:create).with(
+    expect(mailjet_spy).to have_received(:create).with(
       messages: array_including(
-        hash_including(
-          To: [{ Email: ENV.fetch('TO_MAIL_ADDRESS', nil) }]
-        )
+        hash_including(To: [{ Email: ENV.fetch('TO_MAIL_ADDRESS', nil) }])
       )
     )
   end
 
   it '本文に名前が含まれていること' do
-    described_class.contact_email(name, email, message)
-    expect(Mailjet::Send).to have_received(:create).with(
+    expect(mailjet_spy).to have_received(:create).with(
       messages: array_including(
-        hash_including(
-          TextPart: include(name)
-        )
+        hash_including(TextPart: include(name))
       )
     )
   end
 
   it '本文にメールアドレスが含まれていること' do
-    described_class.contact_email(name, email, message)
-    expect(Mailjet::Send).to have_received(:create).with(
+    expect(mailjet_spy).to have_received(:create).with(
       messages: array_including(
-        hash_including(
-          TextPart: include(email)
-        )
+        hash_including(TextPart: include(email))
       )
     )
   end
 
   it '本文にメッセージが含まれていること' do
-    described_class.contact_email(name, email, message)
-    expect(Mailjet::Send).to have_received(:create).with(
+    expect(mailjet_spy).to have_received(:create).with(
       messages: array_including(
-        hash_including(
-          TextPart: include(message)
-        )
+        hash_including(TextPart: include(message))
       )
     )
   end
