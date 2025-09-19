f36c0b77c35b8816333e7e439fe8e47f2a73918e
henrry-sakurazaka <bright.lights.21@me.com>
2025-09-19 14:26:16 +0900

    Update contact_mailer_spec.rb

--- spec/mailers/contact_mailer_spec.rb
+++ spec/mailers/contact_mailer_spec.rb
@@ -3,21 +3,30 @@
 require 'rails_helper'
 
 RSpec.describe ContactMailer, type: :mailer do
-  let(:mail) { described_class.contact_email('山田太郎', 'taro@example.com', 'こんにちは') }
+  let(:name) { '山田太郎' }
+  let(:mail) { described_class.contact_email(name, email, message) }
+  let(:email) { 'taro@example.com' }
+  let(:message) { 'こんにちは' }
+
+  before do
+    # Mailjet API呼び出しをモック
+    allow(Mailjet::Send).to receive(:create).and_return(true)
+  end
+
 
   it '正しい宛先に送信されること' do
     expect(mail.to).to eq([ENV.fetch('TO_MAIL_ADDRESS', nil)])
   end
 
   it '本文に名前が含まれていること' do
-    expect(mail.body.encoded).to include('山田太郎')
+    expect(mail.body.encoded).to include(name)
   end
 
   it '本文にメールアドレスが含まれていること' do
-    expect(mail.body.encoded).to include('taro@example.com')
+    expect(mail.body.encoded).to include(email)
   end
 
   it '本文にメッセージが含まれていること' do
-    expect(mail.body.encoded).to include('こんにちは')
+    expect(mail.body.encoded).to include(message)
   end
 end
