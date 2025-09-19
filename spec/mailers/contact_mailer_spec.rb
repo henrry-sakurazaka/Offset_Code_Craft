292b5c3f10dc1b28e8df4a05dc5a41300613f912
henrry-sakurazaka <bright.lights.21@me.com>
2025-09-19 14:13:10 +0900

    Update contact_mailer_spec.rb

--- spec/mailers/contact_mailer_spec.rb
+++ spec/mailers/contact_mailer_spec.rb
@@ -3,26 +3,21 @@
 require 'rails_helper'
 
 RSpec.describe ContactMailer, type: :mailer do
-  describe '#contact_email' do
-    let(:name) { '山田太郎' }
-    let(:email) { 'test@example.com' }
-    let(:message) { 'これはテストメッセージです。' }
-    let(:mail) { described_class.contact_email(name, email, message) }
+  let(:mail) { described_class.contact_email('山田太郎', 'taro@example.com', 'こんにちは') }
 
-    it '正しい宛先に送信されること' do
-      expect(mail.to).to eq [ENV.fetch('MY_MAIL_ADDRESS', nil)]
-    end
+  it '正しい宛先に送信されること' do
+    expect(mail.to).to eq([ENV.fetch('TO_MAIL_ADDRESS', nil)])
+  end
 
-    it '本文に名前が含まれていること' do
-      expect(mail.body.encoded).to include(name)
-    end
+  it '本文に名前が含まれていること' do
+    expect(mail.body.encoded).to include('山田太郎')
+  end
 
-    it '本文にメールアドレスが含まれていること' do
-      expect(mail.body.encoded).to include(email)
-    end
+  it '本文にメールアドレスが含まれていること' do
+    expect(mail.body.encoded).to include('taro@example.com')
+  end
 
-    it '本文にメッセージが含まれていること' do
-      expect(mail.body.encoded).to include(message)
-    end
+  it '本文にメッセージが含まれていること' do
+    expect(mail.body.encoded).to include('こんにちは')
   end
 end
