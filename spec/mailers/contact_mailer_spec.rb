81847a7299f1172b681fdfa364098d3df2dd4ea4
henrry-sakurazaka <bright.lights.21@me.com>
2025-09-19 14:46:04 +0900

    Update contact_mailer_spec.rb

--- spec/mailers/contact_mailer_spec.rb
+++ spec/mailers/contact_mailer_spec.rb
@@ -4,32 +4,59 @@ require 'rails_helper'
 
 RSpec.describe ContactMailer, type: :mailer do
   let(:name) { '山田太郎' }
-  let(:mail) { described_class.contact_email(name, email, message) }
   let(:email) { 'taro@example.com' }
   let(:message) { 'こんにちは' }
 
   before do
     # 環境変数を設定
+    ENV['FROM_MAIL_ADDRESS'] = 'noreply@offsetcodecraft.site'
     ENV['TO_MAIL_ADDRESS'] = 'admin@example.com'
 
-    # Mailjet APIをモック
-    allow(Mailjet::Send).to receive(:create).and_return(true)
+    # Mailjet::Send.create をモック
+    allow(Mailjet::Send).to receive(:create)
   end
 
-
   it '正しい宛先に送信されること' do
-    expect(mail[:to].value).to eq([ENV.fetch('TO_MAIL_ADDRESS', nil)])
+    described_class.contact_email(name, email, message)
+    expect(Mailjet::Send).to have_received(:create).with(
+      messages: array_including(
+        hash_including(
+          To: [{ Email: ENV.fetch('TO_MAIL_ADDRESS', nil) }]
+        )
+      )
+    )
   end
 
   it '本文に名前が含まれていること' do
-    expect(mail[:textpart].value).to include(name)
+    described_class.contact_email(name, email, message)
+    expect(Mailjet::Send).to have_received(:create).with(
+      messages: array_including(
+        hash_including(
+          TextPart: include(name)
+        )
+      )
+    )
   end
 
   it '本文にメールアドレスが含まれていること' do
-    expect(mail[:textpart].value).to include(email)
+    described_class.contact_email(name, email, message)
+    expect(Mailjet::Send).to have_received(:create).with(
+      messages: array_including(
+        hash_including(
+          TextPart: include(email)
+        )
+      )
+    )
   end
 
   it '本文にメッセージが含まれていること' do
-    expect(mail[:textpart].value).to include(message)
+    described_class.contact_email(name, email, message)
+    expect(Mailjet::Send).to have_received(:create).with(
+      messages: array_including(
+        hash_including(
+          TextPart: include(message)
+        )
+      )
+    )
   end
 end
