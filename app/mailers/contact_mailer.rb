e95a5942f607ef84b95e11b7b041de2dc5878c20
henrry-sakurazaka <bright.lights.21@me.com>
2025-09-19 17:51:26 +0900

    latest version

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,19 +1,20 @@
 # frozen_string_literal: true
 
 # メールフォームの基本設定
+# app/mailers/contact_mailer.rb
 class ContactMailer < ApplicationMailer
   def contact_email(name, email, message)
-    Mailjet::Send.create(messages: [{
-                           From: {
-                             Email: ENV.fetch('FROM_MAIL_ADDRESS', nil),
-                             Name: 'Offset_code_craft'
-                           },
-                           To: [{
-                             Email: ENV.fetch('TO_MAIL_ADDRESS', nil) # 管理者のアドレスとか
-                           }],
-                           Subject: "【お問い合わせ】#{name}様より",
-                           TextPart: "From: #{email}\n\n#{message}",
-                           HTMLPart: "<p>From: #{email}</p><p>#{message}</p>"
-                         }])
+    {
+      From: {
+        Email: ENV.fetch('FROM_MAIL_ADDRESS', nil),
+        Name: 'Offset_code_craft'
+      },
+      To: [{
+        Email: ENV.fetch('TO_MAIL_ADDRESS', nil)
+      }],
+      Subject: "【お問い合わせ】#{name}様より",
+      TextPart: "From: #{email}\n\n#{message}",
+      HTMLPart: "<p>From: #{email}</p><p>#{message}</p>"
+    }
   end
 end
