1bbd46e43a82e585b79a0f5612716c48037ca4a3
henrry-sakurazaka <bright.lights.21@me.com>
2025-09-19 18:19:13 +0900

    Update contact_mailer_spec.rb

--- spec/mailers/contact_mailer_spec.rb
+++ spec/mailers/contact_mailer_spec.rb
@@ -1,15 +1,15 @@
 # frozen_string_literal: true
 
+# spec/mailers/contact_mailer_spec.rb
 require 'rails_helper'
 
 RSpec.describe ContactMailer, type: :mailer do
   let(:name) { '山田太郎' }
   let(:email) { 'taro@example.com' }
   let(:message) { 'こんにちは' }
-  let(:to_mail_address) { 'admin@example.com' }
+  let(:to_mail_address) { ENV.fetch('TO_MAIL_ADDRESS', 'admin@example.com') }
 
-  # ここで Mailjet::Send を差し替える
-  let(:mailjet_spy) { class_double(Mailjet::Send, create: true) }
+  let(:mailjet_spy) { class_spy(Mailjet::Send) }
 
   before do
     stub_const('Mailjet::Send', mailjet_spy)
